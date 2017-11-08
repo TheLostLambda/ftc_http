ce128b82125c6e56b28b27f0e4fc5a838a63b847
Brooks J Rady <b.j.rady@gmail.com>
2017-11-08 01:29:20 -0700

    Version 1.0.0 release

--- src/lib.rs
+++ src/lib.rs
@@ -2,24 +2,66 @@ extern crate reqwest;
 extern crate walkdir;
 
 use std::io::{Read, Write, stdout};
-use walkdir::DirEntry;
-use walkdir::WalkDir;
+use walkdir::{WalkDir, DirEntry};
 use reqwest::*;
-use std::env;
 use std::fs;
 
-// At some point check if this host is reachable
-static HOST: &'static str = "http://192.168.49.1:8080";
+static HOST: &'static str = "http://192.168.49.1:8080"; //FIXME: Check if this is reachable
 
-pub fn clean() {
-    // Potentially add an arguement to this function so specific directories can be wiped, keeping the others.
+pub fn up(src: &std::path::Path) {
     let phone = Client::new();
-    print!("Wiping all remote files...");
-    phone.post(&(HOST.to_string() + "/java/file/delete"))
-         .form(&[("delete","[\"src\"]")])
+
+    fn is_src_file(entry: &DirEntry) -> bool {
+        entry.file_name()
+             .to_str()
+             .unwrap()
+             .contains(".java")
+    }
+
+    let src_tree = WalkDir::new(&src).into_iter()
+                                     .filter_map(|f| f.ok())
+                                     .filter(is_src_file);
+
+    for file in src_tree.map(|f| f.path().to_owned()) {
+        print!("Pushing {}...", file.display());
+        let upload = multipart::Form::new().file("file", file)
+                                           .expect("Failed to open file for uploading.");
+        phone.post(&(HOST.to_string() + "/java/file/upload"))
+             .multipart(upload)
+             .send()
+             .expect("HTTP request failed");
+        println!("done");
+    }
+}
+
+pub fn down(dest: &std::path::Path) {
+    let phone = Client::new();
+
+    let mut tree = String::new();
+
+    phone.get(&(HOST.to_string() + "/java/file/tree"))
          .send()
-         .expect("HTTP request failed");
-    println!("done");
+         .expect("HTTP request failed")
+         .read_to_string(&mut tree)
+         .expect("Couldn't read HTTP response");
+
+    for file in tree.split("\"").filter(|file| file.contains(".java")) {
+        print!("Pulling {}...", file);
+        let filepath = dest.join(&file[1..]);
+        fs::DirBuilder::new().recursive(true)
+                             .create(filepath.parent().unwrap())
+                             .expect("Creating a new directory failed");
+        let mut file_handle = fs::File::create(&filepath).expect("Creating a new file failed");
+        let mut file_data = String::new();
+        phone.get(&(HOST.to_string() + "/java/file/download?f=/src" + file))
+             .send()
+             .expect("HTTP request failed")
+             .read_to_string(&mut file_data)
+             .expect("Couldn't read HTTP response");
+        file_handle.write_all(file_data.as_bytes())
+                   .expect("Writing to file failed");
+        println!("done");
+    }
 }
 
 pub fn build() {
@@ -63,64 +105,13 @@ pub fn build() {
     }
 }
 
-pub fn up(source_dir: &str) {
+pub fn wipe() {
+    // Potentially add an arguement to this function so specific directories can be wiped, keeping the others.
     let phone = Client::new();
-
-    let pwd = env::current_dir().expect("It is not possible to access the current directory");
-    let src = pwd.join(&source_dir);
-
-    fn is_src_file(entry: &DirEntry) -> bool {
-        entry.file_name()
-             .to_str()
-             .unwrap()
-             .contains(".java")
-    }
-
-    let src_tree = WalkDir::new(&src).into_iter()
-                                     .filter_map(|f| f.ok())
-                                     .filter(is_src_file);
-
-    for file in src_tree.map(|f| f.path().to_owned()) {
-        print!("Pushing {}...", file.display());
-        let upload = multipart::Form::new().file("file", file)
-                                           .expect("Failed to open file for uploading.");
-        phone.post(&(HOST.to_string() + "/java/file/upload"))
-             .multipart(upload)
-             .send()
-             .expect("HTTP request failed");
-        println!("done");
-    }
-}
-
-pub fn down(destination_dir: &str) {
-    let phone = Client::new();
-
-    let pwd = env::current_dir().expect("It is not possible to access the current directory");
-    let dest = pwd.join(&destination_dir);
-
-    let mut tree = String::new();
-
-    phone.get(&(HOST.to_string() + "/java/file/tree"))
+    print!("Wiping all remote files...");
+    phone.post(&(HOST.to_string() + "/java/file/delete"))
+         .form(&[("delete","[\"src\"]")])
          .send()
-         .expect("HTTP request failed")
-         .read_to_string(&mut tree)
-         .expect("Couldn't read HTTP response");
-
-    for file in tree.split("\"").filter(|file| file.contains(".java")) {
-        print!("Pulling {}...", file);
-        let filepath = dest.join(&file[1..]);
-        fs::DirBuilder::new().recursive(true)
-                             .create(filepath.parent().unwrap())
-                             .expect("Creating a new directory failed");
-        let mut file_handle = fs::File::create(&filepath).expect("Creating a new file failed");
-        let mut file_data = String::new();
-        phone.get(&(HOST.to_string() + "/java/file/download?f=/src" + file))
-             .send()
-             .expect("HTTP request failed")
-             .read_to_string(&mut file_data)
-             .expect("Couldn't read HTTP response");
-        file_handle.write_all(file_data.as_bytes())
-                   .expect("Writing to file failed");
-        println!("done");
-    }
+         .expect("HTTP request failed");
+    println!("done");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,101 @@
 extern crate ftc_http;
 
+use std::iter::Peekable;
+use std::env;
+
+static VERISON_STR: &'static str = "v1.0.0";
+
 fn main() {
-    ftc_http::down("");
-    ftc_http::clean();
-    ftc_http::up("");
-    ftc_http::build();
+    let mut args = env::args().skip(1).peekable();
+
+    loop {
+        match args.next() {
+            Some(arg) => {
+                if is_option(&arg) {
+                    handle_options(&mut args, arg)
+                } else {
+                    help();
+                    break;
+                }
+            },
+            None => break,
+        };
+    }
+}
+
+fn is_option(arg: &str) -> bool {
+    arg.chars().nth(0).unwrap_or(' ') == '-'
+}
+
+fn handle_options<I: Iterator<Item = String>>(args: &mut Peekable<I>, options: String) {
+    let pwd = env::current_dir().expect("It is not possible to access the current directory");
+    let empty_peek = &String::new();
+    for option in options.chars().skip(1) {
+        match option {
+            'd' => {
+                {
+                    let next_arg = args.peek().unwrap_or(empty_peek);
+                    if !is_option(&next_arg) {
+                        ftc_http::down(&pwd.join(&next_arg));
+                    } else {
+                        ftc_http::down(&pwd);
+                        continue;
+                    }
+                }
+                args.next();
+            },
+            'u' => {
+                {
+                    let next_arg = args.peek().unwrap_or(empty_peek);
+                    if !is_option(&next_arg) {
+                        ftc_http::up(&pwd.join(&next_arg));
+                    } else {
+                        ftc_http::up(&pwd);
+                        continue;
+                    }
+                }
+                args.next();
+            },
+            'b' => ftc_http::build(),
+            'w' => ftc_http::wipe(),
+            'v' => version(),
+             _  => {
+                 help();
+                 break;
+             },
+        };
+    }
+}
+
+fn version() {
+    print!("\
+ftc_http {}
+", VERISON_STR);
+}
+
+fn help() {
+    print!("\
+Usage: ftc_http [OPTION]... [FILE]
+Provides an interface to FTC OnBotJava without being constrained to a browser.
+Actions will be executed in the same order the OPTIONs are given.
+
+Startup:
+  -v          Display this version of ftc_http
+  -h          Display this help text
+
+Actions:
+  -d [DEST]   Downloads the source tree from the robot controller and saves it
+                to the location specified by DEST. If no DEST is given, it
+                defaults to the current directory.
+  -u [SRC]    Uploads .java files to the robot controller. SRC specifies the
+                directory in which to search for the Java files. If no SRC is
+                given, it defaults to the current directory.
+  -b          Initiates a build on the robot controller and reports the build
+                status and any errors back the the user.
+  -w          Wipes all files from the robot controller. Be sure to run
+                ftc_http with the -d option first if you wish to keep any of
+                the files on the robot controller.
+
+Please report any bugs here: https://github.com/TheLostLambda/ftc_http
+");
 }
